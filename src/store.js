2a563a6e7e3f070e0c28644e3599e52487d6e334
Semir N <semirnik2@gmail.com>
2019-03-20 23:53:16 -0400

    Added a temp store

---
+++ src/store.js
@@ -0,0 +1,10 @@
+export const muscles = ["shoulders", "chest", "arms", "back", "legs"];
+
+export const exercise = [
+  {
+    id: "",
+    title: "",
+    description: "",
+    muscles: ""
+  }
+];
